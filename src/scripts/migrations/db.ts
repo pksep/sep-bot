aa2208bdf5ab119a8c9c2eb7f53b21b72782570b
Denis <denis@example.com>
2026-06-25 18:40:44 +0500

    fix: initialize migrations sequelize with explicit postgres dialect

--- src/scripts/migrations/db.ts
+++ src/scripts/migrations/db.ts
@@ -2,6 +2,7 @@
 import { Sequelize } from 'sequelize';
 import * as dotenv from 'dotenv';
 import configFactory from '../../configs/env.config';
+import { URL } from 'url';
 
 dotenv.config({
   path: `env/.${process.env.NODE_ENV}.env`
@@ -9,10 +10,18 @@ dotenv.config({
 
 export function initializeSequelize() {
   const { url } = configFactory().database;
+  const databaseUrl = new URL(url);
 
-  const sequelize = new Sequelize(url, {
-    dialect: 'postgres'
-  });
+  const sequelize = new Sequelize(
+    databaseUrl.pathname.replace('/', ''),
+    databaseUrl.username,
+    databaseUrl.password,
+    {
+      dialect: 'postgres',
+      host: databaseUrl.hostname,
+      port: Number(databaseUrl.port || 5432)
+    }
+  );
 
   return sequelize;
 }
